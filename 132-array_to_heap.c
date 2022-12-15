ace99cc0fc674738d9981fe5011c39ca6ff19105
Kero Filicha <kerotulu@gmail.com>
2022-12-15 12:51:13 -0800

    Heap - Array to Binary Heap

--- 132-array_to_heap.c
+++ 132-array_to_heap.c
@@ -1,11 +1,20 @@
 #include "binary_trees.h"
 /**
- * array_to_heap -function create a heap tree from array
- * @array: array reference
- * @size: size of array
- * Return: pointer to tree
+ * array_to_heap - Turns an array to a tree.
+ *
+ * @array: Array to be transformed.
+ * @size: Size of the array.
+ * Return: The tree of the array.
  */
+
 heap_t *array_to_heap(int *array, size_t size)
 {
-	return (NULL);
+	size_t i;
+	heap_t *root = NULL;
+
+	for (i = 0; i < size; i++)
+	{
+		heap_insert(&root, array[i]);
+	}
+	return (root);
 }
