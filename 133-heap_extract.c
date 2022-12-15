736f87acedf4603624721e70704af51de1aa314a
kerotulu <106761298+kerotulu@users.noreply.github.com>
2022-12-16 00:07:10 +0300

    Update 133-heap_extract.c

--- 133-heap_extract.c
+++ 133-heap_extract.c
@@ -1,174 +1,196 @@
 #include "binary_trees.h"
 
+#define INIT_NODE {0, NULL, NULL, NULL}
+
+#define CONVERT "0123456789ABCDEF"
+
+#define SETUP_NODE_BLOC { \
+	tmp = *root; \
+	size = binary_tree_size(*root); \
+	binary = &buffer[49]; \
+	*binary = 0; \
+	}
+
+#define FREE_NODE_BLOC { \
+		res = tmp->n; \
+		free(tmp); \
+		*root = NULL; \
+	}
+
+#define SWAP_HEAD_BLOC { \
+		head = *root; \
+		head = swap_head(head, tmp); \
+		res = head->n; \
+		free(head); \
+		*root = tmp; \
+		tmp = perc_down(tmp); \
+		*root = tmp; \
+	}
+
+#define CONVERT_LOOP { \
+		*--binary = CONVERT[size % 2]; \
+		size /= 2; \
+	}
+
 /**
- * binary_tree_node - Insert a new node in the tree.
- *
- * @parent: Parent node.
- * @value: Value of the node.
- *
- * Return: Always 0 (Success)
+ * swap - swaps two nodes in binary tree
+ * @a: first node
+ * @b: second node
+ * Return: pointer to root
  */
-
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+bst_t *swap(bst_t *a, bst_t *b)
 {
-	binary_tree_t *new_node;
+	bst_t a_copy = INIT_NODE;
 
-	new_node = malloc(sizeof(binary_tree_t));
-	if (new_node == NULL)
+	a_copy.n = a->n;
+	a_copy.parent = a->parent;
+	a_copy.left = a->left;
+	a_copy.right = a->right;
+	a->parent = b;
+	a->left = b->left;
+	a->right = b->right;
+	if (b->left)
+		b->left->parent = a;
+	if (b->right)
+		b->right->parent = a;
+
+	b->parent = a_copy.parent;
+	if (a_copy.parent)
+	{
+		if (a == a_copy.parent->left)
+			a_copy.parent->left = b;
+		else
+			a_copy.parent->right = b;
+	}
+	if (b == a_copy.left)
+	{
+		b->left = a;
+		b->right = a_copy.right;
+		if (a_copy.right)
+			a_copy.right->parent = b;
+	}
+	else if (b == a_copy.right)
+	{
+		b->right = a;
+		b->left = a_copy.left;
+		if (a_copy.left)
+			a_copy.left->parent = b;
+	}
+	while (b->parent)
+		b = b->parent;
+	return (b);
+}
+
+/**
+ * binary_tree_size - measures the size of a binary tree
+ * @tree: input binary tree
+ * Return: number of descendant child nodes
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+
+	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
+}
+
+/**
+ * swap_head - helper func to swap head and node
+ * @head: pointer to head
+ * @node: pointer to node
+ * Return: pointer to severed head of tree
+ */
+heap_t *swap_head(heap_t *head, heap_t *node)
+{
+	if (node->parent->left == node)
+	{
+		node->parent->left = NULL;
+	} else
+		node->parent->right = NULL;
+	node->parent = NULL;
+	node->left = head->left;
+	node->right = head->right;
+	if (head->left)
+		head->left->parent = node;
+	if (head->right)
+		head->right->parent = node;
+	return (head);
+}
+
+/**
+ * perc_down - percolate head into correct position
+ * @node: pointer to head
+ * Return: pointer to head of tree
+ */
+heap_t *perc_down(heap_t *node)
+{
+	int max;
+	heap_t *next = node;
+
+	if (!node)
 		return (NULL);
-	new_node->n = value;
-	new_node->left = NULL;
-	new_node->right = NULL;
-	new_node->parent = parent;
-	return (new_node);
+	max = node->n;
+	if (node->left)
+		max = MAX(node->left->n, max);
+	if (node->right)
+		max = MAX(node->right->n, max);
+	if (node->left && max == node->left->n)
+		next = node->left;
+	else if (node->right && max == node->right->n)
+		next = node->right;
+	if (next != node)
+	{
+		swap(node, next);
+		perc_down(node);
+	}
+	return (next);
 }
 
 /**
- * check_new_root - checks the new root
- * @root: root
- * @new_root: new root
- * Return: node
- */
-heap_t *check_new_root(heap_t **root, heap_t *new_root)
-{
-	heap_t *head, *current;
-
-	current = *root;
-	while (1)
-	{
-		if (binary_tree_balance(current))
-			current = current->left;
-		else if (!binary_tree_balance(current) && current->right)
-			current = current->right;
-		else if (!binary_tree_balance(current))
-			break;
-	}
-	new_root = current, head = *root;
-	if (new_root == head)
-		free(head), new_root = NULL;
-	else if (new_root->parent == head)
-	{
-		if (head->right == new_root)
-		{
-			new_root->left = head->left, head->left->parent = new_root;
-			*root = new_root, free(head);
-		}
-		else
-		{
-			*root = new_root, free(head);
-			new_root->parent = NULL;
-			return (NULL);
-		}
-	}
-	else
-	{
-		new_root->left = head->left, new_root->left->parent = new_root;
-		new_root->right = head->right, new_root->right->parent = new_root;
-		if (new_root->parent->right == new_root)
-			new_root->parent->right = NULL;
-		else
-			new_root->parent->left = NULL;
-		*root = new_root, free(head);
-	}
-	return (new_root);
-}
-/**
- * if_check - if statement
- * @new_root: new root
- * @tmp: position
- * @current: position
- * Return: void
- */
-void if_check(heap_t *new_root, heap_t *tmp, heap_t *current)
-{
-	if (new_root->right == tmp)
-	{
-		current = tmp->left, tmp->left = new_root->left;
-		if (tmp->left)
-			tmp->left->parent = tmp;
-		new_root->left = current;
-		if (new_root->left)
-			new_root->left->parent = new_root;
-		current = tmp->right, tmp->right = new_root;
-		new_root->right = current;
-	}
-	else
-	{
-		current = tmp->right, tmp->right = new_root->right;
-		if (tmp->right)
-			tmp->right->parent = tmp;
-		new_root->right = current;
-		if (new_root->right)
-			new_root->right->parent = new_root;
-		current = tmp->left, tmp->left = new_root;
-		new_root->left = current;
-	}
-}
-
-/**
- * loop_heap - loop
- * @root: root
- * @new_root: new root
- *
- * Return: void
- */
-void loop_heap(heap_t **root, heap_t *new_root)
-{
-	heap_t *tmp, *current = NULL;
-
-	while (new_root && (new_root->right || new_root->left))
-	{
-		if (new_root->right && new_root->left)
-		{
-			if (new_root->right->n > new_root->left->n)
-				tmp = new_root->right;
-			else
-				tmp = new_root->left;
-		}
-		else if (new_root->left)
-			tmp = new_root->left;
-		else
-			tmp = new_root->right;
-		if (tmp->n > new_root->n)
-		{
-			if_check(new_root, tmp, current);
-			if (current)
-				current->parent = new_root;
-
-			tmp->parent = new_root->parent;
-			if (new_root->parent && new_root->parent->right == new_root)
-				new_root->parent->right = tmp;
-			else if (new_root->parent)
-				new_root->parent->left = tmp;
-			new_root->parent = tmp;
-			if (tmp && !tmp->parent)
-				*root = tmp;
-		}
-		else
-			break;
-	}
-}
-
-/**
- * heap_extract - removes head
- * @root: root
- *
- * Return: value in root
+ * heap_extract - extracts the root node of a Max Binary Heap
+ * @root: double pointer to root of tree
+ * Return: value stored in the root node
  */
 int heap_extract(heap_t **root)
 {
-	heap_t *new_root = NULL;
-	int value;
+	size_t size, i;
+	char *binary, c, buffer[50];
+	int res;
+	heap_t *tmp, *head;
 
-	if (!*root)
+	if (!root || !*root)
 		return (0);
+	SETUP_NODE_BLOC;
+	if (size == 1)
+	{
+		FREE_NODE_BLOC;
+		return (res);
+	}
+	do {
+		CONVERT_LOOP;
+	} while (size);
 
-	value = (*root)->n;
-	new_root = check_new_root(root, new_root);
-	if (!new_root)
-		return (value);
-	new_root->parent = NULL;
-
-	loop_heap(root, new_root);
-	return (value);
+	for (i = 1; i < strlen(binary); i++)
+	{
+		c = binary[i];
+		if (i == strlen(binary) - 1)
+		{
+			if (c == '1')
+			{
+				tmp = tmp->right;
+				break;
+			}
+			else if (c == '0')
+			{
+				tmp = tmp->left;
+				break;
+			}
+		}
+		if (c == '1')
+			tmp = tmp->right;
+		else if (c == '0')
+			tmp = tmp->left;
+	}
+	SWAP_HEAD_BLOC;
+	return (res);
 }
